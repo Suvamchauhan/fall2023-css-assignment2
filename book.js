c1791c1b43754086f1b1af98f3699a24f0533a4f
Suvamchauhan <113388990+Suvamchauhan@users.noreply.github.com>
2023-11-17 23:14:28 -0500

    Update book.js

--- book.js
+++ book.js
@@ -1,20 +1,16 @@
-// Add your custom JavaScript code here
+.custom-navbar-bg {
+    background-color: #2c3e50; /* Your custom color */
+    /* You can add more styling properties as needed */
+    /* For example: */
+    /* color: white; */
+    /* font-family: Arial, sans-serif; */
+  }
 
-// Highlight active section in navigation on scroll
-document.addEventListener('scroll', function() {
-    let sections = document.querySelectorAll('.mt-4'); // Adjust the class if needed
-    let scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-  
-    sections.forEach((section, index) => {
-      let topOffset = section.offsetTop - 50; // Adjust the offset if needed
-  
-      if (scrollPosition >= topOffset) {
-        document.querySelectorAll('.nav-link').forEach((link) => {
-          link.classList.remove('active');
-        });
-  
-        document.querySelectorAll('.nav-link')[index].classList.add('active');
-      }
-    });
-  });
+body{
+    color: white;
+}
+
+  .navbar-expand-lg{
+    background-color: white;
+  }
   
