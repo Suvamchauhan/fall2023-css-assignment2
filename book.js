34700f71a21a32439c20e076b0b97b646c064c2a
Suvamchauhan <113388990+Suvamchauhan@users.noreply.github.com>
2023-11-20 18:39:15 -0500

    Update book.js

--- book.js
+++ book.js
@@ -1,16 +1,2 @@
-.custom-navbar-bg {
-    background-color: #2c3e50; /* Your custom color */
-    /* You can add more styling properties as needed */
-    /* For example: */
-    /* color: white; */
-    /* font-family: Arial, sans-serif; */
-  }
+//  custom JavaScript code here
 
-body{
-    color: white;
-}
-
-  .navbar-expand-lg{
-    background-color: white;
-  }
-  
